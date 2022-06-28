45652eb7adbf088c8659ab7c52542f784894fb2c
lk23286 <lk23286@gmail.com>
2022-06-28 17:22:02 +0200

    Initial Commit

---
+++ Milk and Cow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milk and Cow
+//
+//  Created by Laszlo Kovacs on 2022. 06. 28..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
